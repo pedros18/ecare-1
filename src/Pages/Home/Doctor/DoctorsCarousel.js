8c45ffa27fef8f8e71a40e9937d6d2e6331d4fd4
pedros18 <badrofcb2004@gmail.com>
2024-09-17 12:02:01 +0100

    solving css problems

--- src/Pages/Home/Doctor/DoctorsCarousel.js
+++ src/Pages/Home/Doctor/DoctorsCarousel.js
@@ -11,8 +11,8 @@ const DoctorsCarousel = ({ doctors }) => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
-      setIsAverage(window.innerWidth > 768 && window.innerWidth <= 1200); // Adjust the breakpoint as needed
+      setIsMobile(window.innerWidth <= 768); // Breakpoint for mobile
+      setIsAverage(window.innerWidth > 768 && window.innerWidth <= 1200); // Breakpoint for average
     };
 
     handleResize(); // Check initial width
@@ -26,32 +26,22 @@ const DoctorsCarousel = ({ doctors }) => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 1000, // Adjust the speed of transition
-    slidesToShow: isMobile ? 1 : isAverage ? 2 : 4,
+    speed: 1000, // Speed of transition
+    slidesToShow: isMobile ? 1 : isAverage ? 2 : 4, // Adjust the number of slides based on screen size
     slidesToScroll: 1,
     autoplay: true, // Enable autoplay
     autoplaySpeed: 2500 // Set autoplay speed to 2.5 seconds
   };
 
   return (
-    <div>
-      {isMobile || isAverage ? (
-        <Slider {...settings}>
-          {doctors.map((doctor) => (
-            <div key={doctor.id}>
-              <Doctor doctor={doctor} />
-            </div>
-          ))}
-        </Slider>
-      ) : (
-        <div className="doctors-carousel">
-          {doctors.map((doctor) => (
-            <div key={doctor.id} className="doctor-item">
-              <Doctor doctor={doctor} />
-            </div>
-          ))}
-        </div>
-      )}
+    <div className="doctors-carousel">
+      <Slider {...settings}>
+        {doctors.map((doctor) => (
+          <div key={doctor.id} className="doctor-item">
+            <Doctor doctor={doctor} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
