334d5458e68c31637ae14e7c929b2274441fba5d
pedros18 <badrofcb2004@gmail.com>
2024-09-17 11:23:33 +0100

    Normalize line endings

--- src/Pages/Home/Doctor/DoctorsCarousel.js
+++ src/Pages/Home/Doctor/DoctorsCarousel.js
@@ -7,24 +7,14 @@ import './DoctorsCarousel.css'; // Import CSS file
 
 const DoctorsCarousel = ({ doctors }) => {
   const [isMobile, setIsMobile] = useState(false);
-<<<<<<< HEAD
   const [isAverage, setIsAverage] = useState(false);
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
       setIsAverage(window.innerWidth > 768 && window.innerWidth <= 1200); // Adjust the breakpoint as needed
     };
 
-
-
-=======
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
-    };
-
->>>>>>> origin/main
     handleResize(); // Check initial width
     window.addEventListener('resize', handleResize);
     
@@ -37,36 +27,15 @@ const DoctorsCarousel = ({ doctors }) => {
     dots: true,
     infinite: true,
     speed: 1000, // Adjust the speed of transition
-<<<<<<< HEAD
     slidesToShow: isMobile ? 1 : isAverage ? 2 : 4,
-=======
-    slidesToShow: 4,
->>>>>>> origin/main
     slidesToScroll: 1,
     autoplay: true, // Enable autoplay
-    autoplaySpeed: 2500 // Set autoplay speed to 3 seconds
+    autoplaySpeed: 2500 // Set autoplay speed to 2.5 seconds
   };
 
   return (
     <div>
-<<<<<<< HEAD
-      <Slider {...settings}>
-        {doctors.map((doctor) => (
-          <div key={doctor.id}>
-            <Doctor doctor={doctor} />
-          </div>
-        ))}
-      </Slider>
-=======
-      {isMobile ? (
-        <div className="doctors-carousel">
-          {doctors.map((doctor) => (
-            <div key={doctor.id} className="doctor-item">
-              <Doctor doctor={doctor} />
-            </div>
-          ))}
-        </div>
-      ) : (
+      {isMobile || isAverage ? (
         <Slider {...settings}>
           {doctors.map((doctor) => (
             <div key={doctor.id}>
@@ -74,8 +43,15 @@ const DoctorsCarousel = ({ doctors }) => {
             </div>
           ))}
         </Slider>
+      ) : (
+        <div className="doctors-carousel">
+          {doctors.map((doctor) => (
+            <div key={doctor.id} className="doctor-item">
+              <Doctor doctor={doctor} />
+            </div>
+          ))}
+        </div>
       )}
->>>>>>> origin/main
     </div>
   );
 };
